70bf431f45359f06a3b58958c58b7b40a524da31
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-22 13:18:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_4_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //decalaration
+            string name = "John";
+            int age = 35;
+            bool isTall = false;
+            double weight = 80.2;
+
+            /*for the smallest least precise decimal use flaot,
+             * in most cases double will do just fine,
+             *  */
+
+            Console.WriteLine(name + " is " + age + " he weighs " + weight);
+            Console.WriteLine("it is " + isTall + " that he is tall");
+
+            //freeze console
+            Console.ReadLine();
+
+        }
+    }
+}
